a44877b57b026382f897b3bb6074c6e443105717
Vova Dranyy <v.dranyy@gmail.com>
2016-09-04 19:00:40 +0300

    add validation

--- crimemap.py
+++ crimemap.py
@@ -1,4 +1,7 @@
+import datetime
+import dateparser
 import json
+import string
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -12,41 +15,44 @@ else:
 
 app = Flask(__name__)
 DB = DBHelper()
+CATEGORIES = ['mugging', 'break-in']
+
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, '%Y-%m-%d')
+    except TypeError:
+        return None
+
+
+def sanitize_string(userinput):
+    whitelist = string.ascii_letters + string.digits + " !?$.,:;'-()&"
+    return filter(lambda x: x in whitelist, userinput)
 
 
 @app.route('/')
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template('home.html', crimes=crimes)
-
-
-@app.route('/add', methods=['POST'])
-def add():
-    try:
-        data = request.form.get('userinput')
-        DB.add_input(data)
-    except Exception as e:
-        print(e)
-    return home()
-
-
-@app.route('/clear')
-def clear():
-    try:
-        DB.clear_all()
-    except Exception as e:
-        print(e)
-    return home()
+    return render_template('home.html', crimes=crimes, 
+            categories=CATEGORIES, error_message=error_message)
 
 
 @app.route('/submitcrime', methods=['POST'])
 def submitcrime():
     category = request.form.get('category')
+    if category not in CATEGORIES:
+        return home()
     date = request.form.get('date')
-    latitude = float(request.form.get('latitude'))
-    longitude = float(request.form.get('longitude'))
-    description = request.form.get('description')
+    if not date:
+        return home('Invalid date. Please use yyy-mm-dd format.')
+    try:
+        latitude = float(request.form.get('latitude'))
+        longitude = float(request.form.get('longitude'))
+    except ValueError:
+        return home()
+    description = sanitize_string(request.form.get('description'))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
--- mockdbhelper.py
+++ mockdbhelper.py
@@ -3,15 +3,6 @@ class MockDBHelper:
     def connect(self, database='crimemap'):
         pass
 
-    def get_all_inputs(self):
-        return []
-
-    def add_input(self, data):
-        pass
-
-    def clear_all(self):
-        pass
-
     def add_crime(self, category, date, latitude, longitude, description):
         pass
 
